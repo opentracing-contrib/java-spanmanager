4b09079683bac79897092e6d7c10fcac4e033488
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-01-09 15:01:46 +0100

    Unit test where ManagedSpan was released from other thread.

--- src/test/java/io/opentracing/contrib/spanmanager/DefaultSpanManagerTest.java
+++ src/test/java/io/opentracing/contrib/spanmanager/DefaultSpanManagerTest.java
@@ -116,12 +116,61 @@ public class DefaultSpanManagerTest {
         ManagedSpan managed3 = manager.manage(span3);
         assertThat("pushed span3", manager.currentSpan(), is(sameInstance(span3)));
 
-        // Pop2: Span1 -> Span2(X) -> Span3  :  current span stays Span3
+        // Pop2: Span1 -> Span2(X) -> Span3  :  currentSpan stays Span3
         managed2.release();
         assertThat("released span2", manager.currentSpan(), is(sameInstance(span3)));
 
         managed3.release();
         assertThat("skipped span2 (already-released)", manager.currentSpan(), is(sameInstance(span1)));
+
+        managed1.release();
+        assertThat("popped span1", manager.currentSpan(), is(instanceOf(NoopSpan.class)));
+    }
+
+    /**
+     * <strong>Note:</strong> This is not a normal use-case!<br>
+     * The {@link ManagedSpan} is intended to be created and used in the scope of a try-with-resources block
+     * (so within the scope of a single thread).
+     * <p>
+     * This test is merely here to guarantee predictable behaviour when it happens.
+     */
+    @Test
+    public void testReleaseFromOtherThreads() throws InterruptedException {
+        Span span1 = mock(Span.class);
+        Span span2 = mock(Span.class);
+        Span span3 = mock(Span.class);
+
+        assertThat("empty stack", manager.currentSpan(), is(instanceOf(NoopSpan.class)));
+
+        ManagedSpan managed1 = manager.manage(span1);
+        assertThat("pushed span1", manager.currentSpan(), is(sameInstance(span1)));
+
+        final ManagedSpan managed2 = manager.manage(span2);
+        assertThat("pushed span2", manager.currentSpan(), is(sameInstance(span2)));
+
+        ManagedSpan managed3 = manager.manage(span3);
+        assertThat("pushed span3", manager.currentSpan(), is(sameInstance(span3)));
+
+        // Schedule 10 threads to release managed2
+        Thread[] releasers = new Thread[10];
+        for (int i = 0; i < releasers.length; i++) {
+            releasers[i] = new Thread() {
+                @Override
+                public void run() {
+                    managed2.release();
+                }
+            };
+        }
+
+        // Schedule managed2.release() 10x
+        for (int i = 0; i < releasers.length; i++) releasers[i].start();
+
+        managed3.release();
+
+        // Wait for managed2.releases
+        for (int i = 0; i < releasers.length; i++) releasers[i].join();
+
+        assertThat("popped span2+3", manager.currentSpan(), is(sameInstance(span1)));
 
         managed1.release();
         assertThat("popped span1", manager.currentSpan(), is(instanceOf(NoopSpan.class)));

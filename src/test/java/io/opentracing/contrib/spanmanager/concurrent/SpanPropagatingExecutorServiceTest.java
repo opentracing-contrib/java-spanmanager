a3a4d942bdc4513acee15d81451d98cb737c1cd1
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-01-10 10:59:49 +0100

    Unit test for SpanPropagatingExecutorService.

--- src/test/java/io/opentracing/contrib/spanmanager/concurrent/SpanPropagatingExecutorServiceTest.java
+++ src/test/java/io/opentracing/contrib/spanmanager/concurrent/SpanPropagatingExecutorServiceTest.java
@@ -1,85 +1,150 @@
 package io.opentracing.contrib.spanmanager.concurrent;
 
+import io.opentracing.NoopSpan;
 import io.opentracing.Span;
+import io.opentracing.contrib.spanmanager.DefaultSpanManager;
 import io.opentracing.contrib.spanmanager.SpanManager;
-import org.hamcrest.Matchers;
+import io.opentracing.contrib.spanmanager.SpanManager.ManagedSpan;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.sameInstance;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
 
 public class SpanPropagatingExecutorServiceTest {
 
-    ExecutorService mockExecutorService;
-    SpanManager mockSpanManager;
-    Span mockSpan;
+    static final ExecutorService threadpool = Executors.newCachedThreadPool();
+    static final SpanManager spanManager = DefaultSpanManager.getInstance();
 
-    SpanPropagatingExecutorService service;
+    SpanPropagatingExecutorService subject;
 
     @Before
     public void setUp() {
-        mockExecutorService = mock(ExecutorService.class);
-        mockSpanManager = mock(SpanManager.class);
-        mockSpan = mock(Span.class);
-        when(mockSpanManager.currentSpan()).thenReturn(mockSpan);
-
-        service = new SpanPropagatingExecutorService(mockExecutorService, mockSpanManager);
+        subject = new SpanPropagatingExecutorService(threadpool, spanManager);
+        spanManager.clear();
     }
 
     @After
-    public void verifyMocks() {
-        verifyNoMoreInteractions(mockExecutorService, mockSpanManager, mockSpan);
+    public void tearDown() {
+        spanManager.clear();
+    }
+
+    @AfterClass
+    public static void shutdownThreadpool() {
+        assertThat(threadpool.shutdownNow(), equalTo(Collections.<Runnable>emptyList()));
     }
 
     @Test
-    public void testExecuteRunnable() {
-        Runnable runnable = mock(Runnable.class);
-
-        service.execute(runnable);
-
-        verify(mockSpanManager).currentSpan(); // current span must be propagated
-        verify(mockExecutorService).execute(any(RunnableWithManagedSpan.class)); // into RunnableWithManagedSpan
-        verifyNoMoreInteractions(runnable);
-    }
-
-    @Test
-    public void testSubmitRunnable() {
-        Runnable runnable = mock(Runnable.class);
-        Future future = mock(Future.class);
-        when(mockExecutorService.submit(any(Runnable.class))).thenReturn(future);
-
-        assertThat(service.submit(runnable), is(sameInstance(future)));
-
-        verify(mockSpanManager).currentSpan(); // current span must be propagated.
-        verify(mockExecutorService).submit(any(RunnableWithManagedSpan.class)); // into RunnableWithManagedSpan
-        verifyNoMoreInteractions(runnable, future);
-    }
-
-    @Test
-    public void testConstructorWithoutDelegate() {
+    public void testExecuteRunnable() throws ExecutionException, InterruptedException {
+        CurrentSpanRunnable runnable = new CurrentSpanRunnable();
+        ManagedSpan callerManagedSpan = spanManager.manage(mock(Span.class));
         try {
-            new SpanPropagatingExecutorService(null, mock(SpanManager.class));
-            fail("Exception with message expected.");
-        } catch (RuntimeException expected) {
-            assertThat(expected.getMessage(), is(Matchers.notNullValue()));
+
+            // Execute runnable and wait for completion.
+            FutureTask<Void> future = new FutureTask<Void>(runnable, null);
+            subject.execute(future);
+            future.get();
+
+            assertThat("Current span in thread", runnable.span, is(sameInstance(callerManagedSpan.getSpan())));
+        } finally {
+            callerManagedSpan.release();
         }
     }
 
     @Test
-    public void testConstructorWithoutSpanManager() {
+    public void testSubmitRunnable() throws ExecutionException, InterruptedException {
+        CurrentSpanRunnable runnable = new CurrentSpanRunnable();
+        ManagedSpan callerManagedSpan = spanManager.manage(mock(Span.class));
         try {
-            new SpanPropagatingExecutorService(mock(ExecutorService.class), null);
-            fail("Exception with message expected.");
-        } catch (RuntimeException expected) {
-            assertThat(expected.getMessage(), is(Matchers.notNullValue()));
+
+            subject.submit(runnable).get(); // submit and block.
+            assertThat("Current span in thread", runnable.span, is(sameInstance(callerManagedSpan.getSpan())));
+
+        } finally {
+            callerManagedSpan.release();
+        }
+    }
+
+    @Test
+    public void testSubmitCallable() throws ExecutionException, InterruptedException {
+        Callable<Span> callable = new CurrentSpanCallable();
+        ManagedSpan callerManagedSpan = spanManager.manage(mock(Span.class));
+        try {
+
+            Future<Span> threadSpan = subject.submit(callable);
+            assertThat("Current span in thread", threadSpan.get(), is(sameInstance(callerManagedSpan.getSpan())));
+
+        } finally {
+            callerManagedSpan.release();
+        }
+    }
+
+    @Test
+    public void testInvokeAll() throws ExecutionException, InterruptedException {
+        Collection<Callable<Span>> callables = Arrays.<Callable<Span>>asList(
+                new CurrentSpanCallable(), new CurrentSpanCallable(), new CurrentSpanCallable());
+        ManagedSpan callerManagedSpan = spanManager.manage(mock(Span.class));
+        try {
+
+            List<Future<Span>> futures = subject.invokeAll(callables);
+            assertThat("Futures", futures, hasSize(equalTo(callables.size())));
+            for (Future<Span> threadSpan : futures) {
+                assertThat("Current span in thread", threadSpan.get(), is(sameInstance(callerManagedSpan.getSpan())));
+            }
+
+        } finally {
+            callerManagedSpan.release();
+        }
+    }
+
+
+    @Test
+    public void testExecuteRunnableWithoutCurrentSpan() throws ExecutionException, InterruptedException {
+        CurrentSpanRunnable runnable = new CurrentSpanRunnable();
+
+        // Execute runnable and wait for completion.
+        FutureTask<Void> future = new FutureTask<Void>(runnable, null);
+        subject.execute(future);
+        future.get();
+
+        assertThat("Current span in thread", runnable.span, allOf(notNullValue(), instanceOf(NoopSpan.class)));
+    }
+
+    @Test
+    public void testSubmitRunnableWithoutCurrentSpan() throws ExecutionException, InterruptedException {
+        CurrentSpanRunnable runnable = new CurrentSpanRunnable();
+        subject.submit(runnable).get(); // submit and block.
+        assertThat("Current span in thread", runnable.span, allOf(notNullValue(), instanceOf(NoopSpan.class)));
+    }
+
+    @Test
+    public void testSubmitCallableWithoutCurrentSpan() throws ExecutionException, InterruptedException {
+        Future<Span> threadSpan = subject.submit(new CurrentSpanCallable());
+        assertThat("Current span in thread", threadSpan.get(), allOf(notNullValue(), instanceOf(NoopSpan.class)));
+    }
+
+    static class CurrentSpanRunnable implements Runnable {
+        Span span = null;
+
+        @Override
+        public void run() {
+            span = spanManager.currentSpan();
+        }
+    }
+
+    static class CurrentSpanCallable implements Callable<Span> {
+        @Override
+        public Span call() {
+            return spanManager.currentSpan();
         }
     }
 
